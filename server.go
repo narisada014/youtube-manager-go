71435a7e8045fa359bf728482617f1847904e6b6
narisada014 <nari.reishi@gmail.com>
2019-10-14 23:53:29 +0900

    apiのルーティングを追加

--- server.go
+++ server.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"github.com/labstack/echo"
+	"youtube-manager-go/routes"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
 	e := echo.New()
+
+	e.Use(middleware.Logger())
+
+	routes.Init(e)
 	e.Logger.Fatal(e.Start(":8080"))
 }
